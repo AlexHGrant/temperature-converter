b7786289f19ff8d52da4e681fddeab51fe68fed7
AleczanderGrant <132519462+agrant312@users.noreply.github.com>
2024-08-07 18:36:10 -0400

    Update main.rs
    Reformatted steps and fns for better error handling and unit testing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,177 @@
+use std::ops::Index;
+
+#[derive(Debug)]
+enum Scale {
+    Kelvin,
+    Celsius,
+    Fahrenheit,
+}
+
 fn main() {
     let input = std::env::args().nth(1).expect("Please enter a temperature in F, C or K");
-    let input_value = input[..(input.chars().count() - 1)].parse::<f64>().unwrap();
-    let input_unit = input.chars().nth(input.chars().count() - 1).unwrap().to_uppercase().collect::<Vec<_>>()[0];
-    match input_unit{
-        'K'=>println!("{}{}\n{}{}\n{}{}", input_value, 'K', to_cels(input_value, 'K'), 'C', to_fahr(input_value, 'K'), 'F'),
-        'C'=>println!("{}{}\n{}{}\n{}{}", to_kelv(input_value, 'C'), 'K', input_value, 'C', to_fahr(input_value, 'C'), 'F'),
-        'F'=>println!("{}{}\n{}{}\n{}{}", to_kelv(input_value, 'F'), 'K', to_cels(input_value, 'F'), 'C', input_value, 'F'),
-        _=>println!("Invalid Entry")
+    match calculate(input) {
+        Ok(v) => println! ("{:?}: {}\n{:?}: {}\n{:?}: {}", v.0.0, v.0.1, v.1.0, v.1.1, v.2.0, v.2.1),
+        Err(e) => println!("{}", e)
     }
 }
 
-fn to_cels(value: f64, unit: char) -> f64 {
-    match unit {
-        'K'=> return value - 273.15,
-        'F'=> return (value - 32.0) * 5.0/9.0,
+fn convert(scale: &Scale, value: f32) -> ((Scale, f32), (Scale, f32)) {
+    match scale {
+        Scale::Kelvin => return ((Scale::Celsius, to_cels(&scale, value)), (Scale::Fahrenheit, to_fahr(&scale, value))),
+        Scale::Celsius => return ((Scale::Kelvin, to_kelv(&scale, value)), (Scale::Fahrenheit, to_fahr(&scale, value))),
+        Scale::Fahrenheit => return ((Scale::Celsius, to_cels(&scale, value)), (Scale::Kelvin, to_kelv(&scale, value)))
+    }
+}
+
+fn calculate(input: String) -> Result<((Scale, f32), (Scale, f32), (Scale, f32)), String> {
+    let mut conversions: ((Scale, f32), (Scale, f32)) = ((Scale::Kelvin, 0.0), (Scale::Kelvin, 0.0));
+    let mut r: String = "".to_string();
+    match parse_temp_input(input.as_str()) {
+        Ok(parsedInput) => {
+            conversions = convert(&parsedInput.0, parsedInput.1);
+            return Ok(((parsedInput.0, parsedInput.1), (conversions.0.0, conversions.0.1), (conversions.1.0, conversions.1.1)));
+        },
+        Err(str) => return Err(format!("{}", str))
+    }
+}
+
+fn to_cels(scale: &Scale, value: f32) -> f32 {
+    match scale {
+        Scale::Kelvin => return value - 273.15,
+        Scale::Fahrenheit => return (value - 32.0) * 5.0/9.0,
         _=> return 0.0
     }
 }
 
-fn to_fahr(value: f64, unit: char) -> f64 {
-    match unit {
-        'K'=> return (value - 273.15) * 9.0/5.0 + 32.0,
-        'C'=> return value * 9.0/5.0 + 32.0,
+fn to_fahr(scale: &Scale, value: f32) -> f32 {
+    match scale {
+        Scale::Kelvin => return (value - 273.15) * 9.0/5.0 + 32.0,
+        Scale::Celsius => return value * 9.0/5.0 + 32.0,
         _=> return 0.0
     }
 }
 
-fn to_kelv(value: f64, unit: char) -> f64 {
-    match unit {
-        'F'=> return (value - 32.0) * 5.0/9.0 + 273.15,
-        'C'=> return value + 273.15,
+fn to_kelv(scale: &Scale, value: f32) -> f32 {
+    match scale {
+        Scale::Fahrenheit => return (value - 32.0) * 5.0/9.0 + 273.15,
+        Scale::Celsius => return value + 273.15,
         _=> return 0.0
+    }
+}
+
+fn parse_temp_input(input: &str) -> Result<(Scale, f32), String> {
+    // get all but last character
+    let temp_str = input.chars().take(input.len() - 1).collect::<String>();
+    let temp = match temp_str.parse::<f32>() {
+        Ok(val) => val,
+        Err(_) => {
+            return Err(format!("invalid number {}", temp_str));
+        }
+    };
+
+    // get last character
+    let scale = match input.chars().last() {
+        Some(c) if c == 'c' || c == 'C' => {
+            Scale::Celsius
+        }
+        Some(c) if c == 'f' || c == 'F' => {
+            Scale::Fahrenheit
+        }
+        Some(c) if c == 'k' || c == 'K' => {
+            Scale::Kelvin
+        }
+        Some(c) => {
+            return Err(format!("unknown scale {}", c));
+        }
+        None => {
+            return Err("empty input".to_string());
+        }
+    };
+
+    return Ok((scale, temp));
+}
+
+#[test]
+fn test_calculate() {
+    let inputs = vec![
+        "10c",
+        "10C",
+        "10f",
+        "10F",
+        "10k",
+        "10K",
+        "0c",
+        "0C",
+        "0f",
+        "0F",
+        "0k",
+        "0K",
+        "1234c",
+        "1234C",
+        "1234f",
+        "1234F",
+        "1234k",
+        "1234K",
+        "-10c",
+        "-10C",
+        "-10f",
+        "-10F",
+        "-10k",
+        "-10K",
+        "-0c",
+        "-0C",
+        "-0f",
+        "-0F",
+        "-0k",
+        "-0K",
+        "-1234c",
+        "-1234C",
+        "abc",
+        "-1234G",
+        "-1234k",
+        "-1234K",
+    ];
+
+    let outputs = vec![
+        "10c",
+        "10C",
+        "10f",
+        "10F",
+        "10k",
+        "10K",
+        "0c",
+        "0C",
+        "0f",
+        "0F",
+        "0k",
+        "0K",
+        "1234c",
+        "1234C",
+        "1234f",
+        "1234F",
+        "1234k",
+        "1234K",
+        "-10c",
+        "-10C",
+        "-10f",
+        "-10F",
+        "-10k",
+        "-10K",
+        "-0c",
+        "-0C",
+        "-0f",
+        "-0F",
+        "-0k",
+        "-0K",
+        "-1234c",
+        "-1234C",
+        "abc",
+        "-1234G",
+        "-1234k",
+        "-1234K",
+    ];
+
+    for i in 0..inputs.len() {
+        assert_eq!(calculate(inputs[i].to_string()), outputs[i].to_string());
     }
 }
