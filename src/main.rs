98a94c882140ae147f51b36b2ec0fa5027e0e2be
AleczanderGrant <132519462+agrant312@users.noreply.github.com>
2024-08-08 11:20:59 -0400

    Implemented test_calculate_round
    Corrected some case errors and implemented test_calculate_round to test calculation accuracy to the ten-thousandth decimal

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn convert(scale: &Scale, value: f32) -> ((Scale, f32), (Scale, f32)) {
     match scale {
         Scale::Kelvin => return ((Scale::Celsius, to_cels(&scale, value)), (Scale::Fahrenheit, to_fahr(&scale, value))),
         Scale::Celsius => return ((Scale::Kelvin, to_kelv(&scale, value)), (Scale::Fahrenheit, to_fahr(&scale, value))),
-        Scale::Fahrenheit => return ((Scale::Celsius, to_cels(&scale, value)), (Scale::Kelvin, to_kelv(&scale, value)))
+        Scale::Fahrenheit => return ((Scale::Kelvin, to_kelv(&scale, value)), (Scale::Celsius, to_cels(&scale, value)))
     }
 }
 
@@ -25,9 +25,9 @@ fn calculate(input: String) -> Result<((Scale, f32), (Scale, f32), (Scale, f32))
     let mut conversions: ((Scale, f32), (Scale, f32)) = ((Scale::Kelvin, 0.0), (Scale::Kelvin, 0.0));
     let mut r: String = "".to_string();
     match parse_temp_input(input.as_str()) {
-        Ok(parsedInput) => {
-            conversions = convert(&parsedInput.0, parsedInput.1);
-            return Ok(((parsedInput.0, parsedInput.1), (conversions.0.0, conversions.0.1), (conversions.1.0, conversions.1.1)));
+        Ok(parsed_input) => {
+            conversions = convert(&parsed_input.0, parsed_input.1);
+            return Ok(((parsed_input.0, parsed_input.1), (conversions.0.0, conversions.0.1), (conversions.1.0, conversions.1.1)));
         },
         Err(str) => return Err(format!("{}", str))
     }
@@ -89,10 +89,11 @@ fn parse_temp_input(input: &str) -> Result<(Scale, f32), String> {
     return Ok((scale, temp));
 }
 
-macro_rules! test_calculate {
+macro_rules! test_calculate_round {
     (
         $(
             $test_name:ident : $in:expr => $expected:expr
+
         )+
     ) => {
         $(
@@ -100,9 +101,13 @@ macro_rules! test_calculate {
             fn $test_name() {
                 match calculate($in) {
                     Ok(v) => {
-                        assert_eq!(v.0.1, $expected.0);
-                        assert_eq!(v.1.1, $expected.1);
-                        assert_eq!(v.2.1, $expected.2);
+                        //testing for accuracy to the ten-thousandth of a degree instead of to the full figure due to rounding differences
+                        let ex0 : f32 = $expected.0;
+                        assert!(v.0.1.abs() - ex0.abs() < 0.0001);
+                        let ex1 : f32 = $expected.1;
+                        assert!(v.1.1.abs() - ex1.abs() < 0.0001);
+                        let ex2 : f32 = $expected.2;
+                        assert!(v.2.1.abs() - ex2.abs() < 0.0001);
                     },
                     Err(s) => assert!(false)
                 }
@@ -111,13 +116,43 @@ macro_rules! test_calculate {
     };
 }
 
-test_calculate![
-    test_calculate_0: "10c".to_string() => (10.0, 283.15, 50.0)
-    test_calculate_1: "10C".to_string() => (10.0, 283.15, 50.0)
-    test_calculate_2: "10f".to_string() => (10.0, 260.9278, -12.22222)
-    test_calculate_3: "10F".to_string() => (10.0, 260.9278, -12.22222)
-    test_calculate_4: "10k".to_string() => (10.0, -263.15, -441.67)
-    test_calculate_5: "10K".to_string() => (10.0, -263.15, -441.67)
+test_calculate_round![
+    test_calculate_round_0: "10c".to_string() => (10.0, 283.15, 50.0)
+    test_calculate_round_1: "10C".to_string() => (10.0, 283.15, 50.0)
+    test_calculate_round_2: "10f".to_string() => (10.0, 260.9278, -12.22222)
+    test_calculate_round_3: "10F".to_string() => (10.0, 260.9278, -12.22222)
+    test_calculate_round_4: "10k".to_string() => (10.0, -263.15, -441.67)
+    test_calculate_round_5: "10K".to_string() => (10.0, -263.15, -441.67)
+    test_calculate_round_6: "0c".to_string() => (0.0, 273.15, 32.0)
+    test_calculate_round_7: "0C".to_string() => (0.0, 273.15, 32.0)
+    test_calculate_round_8: "0f".to_string() => (0.0, 255.3722, -17.77778)
+    test_calculate_round_9: "0F".to_string() => (0.0, 255.3722, -17.77778)
+    test_calculate_round_10: "0k".to_string() => (0.0, -273.15, -459.67)
+    test_calculate_round_11: "0K".to_string() => (0.0, -273.15, -459.67)
+    test_calculate_round_12: "1234c".to_string() => (1234.0, 1507.15, 2253.2)
+    test_calculate_round_13: "1234C".to_string() => (1234.0, 1507.15, 2253.2)
+    test_calculate_round_14: "1234f".to_string() => (1234.0, 940.9278, 667.7778)
+    test_calculate_round_15: "1234F".to_string() => (1234.0, 940.9278, 667.7778)
+    test_calculate_round_16: "1234k".to_string() => (1234.0, 960.85, 1761.53)
+    test_calculate_round_17: "1234K".to_string() => (1234.0, 960.85, 1761.53)
+    test_calculate_round_18: "-10c".to_string() => (-10.0, 263.15,14.0)
+    test_calculate_round_19: "-10C".to_string() => (-10.0, 263.15,14.0)
+    test_calculate_round_20: "-10f".to_string() => (-10.0, 249.8167, -23.33333)
+    test_calculate_round_21: "-10F".to_string() => (-10.0, 249.8167, -23.33333)
+    test_calculate_round_22: "-10k".to_string() => (-10.0, -283.15, -477.67)
+    test_calculate_round_23: "-10K".to_string() => (-10.0, -283.15, -477.67)
+    test_calculate_round_24: "-0c".to_string() => (0.0, 273.15, 32.0)
+    test_calculate_round_25: "-0C".to_string() => (0.0, 273.15, 32.0)
+    test_calculate_round_26: "-0f".to_string() => (0.0, 255.3722, -17.77778)
+    test_calculate_round_27: "-0F".to_string() => (0.0, 255.3722, -17.77778)
+    test_calculate_round_28: "-0k".to_string() => (0.0, -273.15, -459.67)
+    test_calculate_round_29: "-0K".to_string() => (0.0, -273.15, -459.67)
+    test_calculate_round_30: "-1234c".to_string() => (-1234.0, -960.85, -2189.2)
+    test_calculate_round_31: "-1234C".to_string() => (-1234.0, -960.85, -2189.2)
+    test_calculate_round_32: "-1234f".to_string() => (-1234.0, -430.1833, -703.3333)
+    test_calculate_round_33: "-1234F".to_string() => (-1234.0, -430.1833, -703.3333)
+    test_calculate_round_34: "-1234k".to_string() => (-1234.0, -1507.15, -2680.87)
+    test_calculate_round_35: "-1234K".to_string() => (-1234.0, -1507.15, -2680.87)
 ];
 
         // "10c",
